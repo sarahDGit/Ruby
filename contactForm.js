52057fee989dd866882972a2f193428d508e8e57
Anne-Marie Serveto <servetonguyen@outlook.com>
2021-06-14 19:54:44 +0200

    modification du texte du JS

--- contactForm.js
+++ contactForm.js
@@ -2,5 +2,5 @@ const buttonElement = document.getElementById("submitbutton");
 
 buttonElement.addEventListener("click", function (event) {
   event.preventDefault();
-  Swal.fire("Good job!", "We'll get in touch soon!", "success");
+  Swal.fire("We'll get in touch soon!", "success");
 });
