70f7d7e03689a899aa1417256cb05bd986230a9e
Louis Dupas <louisdupas@net-c.com>
2021-06-23 13:51:10 +0200

    to top butn fixed

--- scrollToTop.js
+++ scrollToTop.js
@@ -1,9 +1,18 @@
- const btnScrollTop = document.querySelector(".btnScrollTop");
+//  const btnScrollTop = document.querySelector(".btnScrollTop");
 
- window.addEventListener(`scroll`, () => {
-	 if (window.pageYOffset > 100) {
-		btnScrollTop.classList.add(`active`);
-	 }else {
-		btnScrollTop.classList.remove(`active`);
-	 }
-})
+//  window.addEventListener(`scroll`, () => {
+// 	 if (window.pageYOffset > 100) {
+// 		btnScrollTop.classList.add(`active`);
+// 	 }else {
+// 		btnScrollTop.classList.remove(`active`);
+// 	 }
+// })
+
+window.onscroll = function () {
+	const height = document.getElementById('origine').offsetHeight
+	if (window.pageYOffset > height) {
+	  document.getElementById('scrollTop').classList.add('visible');
+	} else {
+	  document.getElementById('scrollTop').classList.remove('visible');
+	}
+  }
