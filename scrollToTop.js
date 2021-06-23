9c61797442ce32a9ba538bd80b8be1ebb269f694
Mo-Oblatif <martinezmoana@gmail.com>
2021-06-23 16:38:09 +0200

    fix ScrollTop btn

--- scrollToTop.js
+++ scrollToTop.js
@@ -1,9 +1,8 @@
- const btnScrollTop = document.querySelector(".btnScrollTop");
-
- window.addEventListener(`scroll`, () => {
-	 if (window.pageYOffset > 100) {
-		btnScrollTop.classList.add(`active`);
-	 }else {
-		btnScrollTop.classList.remove(`active`);
-	 }
-})
+window.onscroll = function () {
+	const height = document.getElementById('origine').offsetHeight
+	if (window.pageYOffset > height) {
+	  document.getElementById('scrollTop').classList.add('visible');
+	} else {
+	  document.getElementById('scrollTop').classList.remove('visible');
+	}
+  }
