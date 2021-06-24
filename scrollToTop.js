73ae462decd70fe151b5784108d6aebd21712395
Peter <peter@MacBook-Air-de-Peter.local>
2021-06-24 17:45:04 +0200

    updt

--- scrollToTop.js
+++ scrollToTop.js
@@ -1,6 +1,6 @@
 window.onscroll = function () {
-	const height = document.getElementById('footer').offsetHeight
-	if (window.pageYOffset > height) {
+	// const height = document.getElementById('footer').offsetHeight
+	if (window.pageYOffset > 800) {
 	  document.getElementById('scrollTop').classList.add('visible');
 	} else {
 	  document.getElementById('scrollTop').classList.remove('visible');
